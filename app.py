0654ff3a8a7429b643b8b776012620e8b5e637a1
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2022-12-12 17:12:13 +0100

    System almost fully built. Model build complete, front end complete, model saved, backend almost complete.

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from crypt import methods
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+# 
+
+
+
 @app.route("/home/", methods=["POST", "GET"])
 def home():
-    return(render_template())
+    if request.method == "GET":
+        return(render_template("index.html"))
+
+    elif request.method == "POST":
+
+        # Collection of data from form.
+        times_pregnant = request.form.get("NoPreg")
+        glucose_concentration = request.form.get("GluCon")
+        bp = request.form.get("BP")
+        skin_fold = request.form.get("SFT")
+        serum_insulin = request.form.get("SIL")
+        bmi = request.form.get("BMI")
+        bp = request.form.get("DP")
+        age = request.form.get("Age")
+        
+
 
 if __name__ == "__main__":
     app.run()
