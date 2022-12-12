a614bb179434a53e119a48fc790cfba7616c7f67
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2022-12-12 17:39:33 +0100

    Model integrated and working, remaining result page.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import pickle
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-# 
+# Deserialization of saved model.
+model = pickle.load(open("final_model.pkl", "rb"))
 
 
 
@@ -21,9 +23,20 @@ def home():
         skin_fold = request.form.get("SFT")
         serum_insulin = request.form.get("SIL")
         bmi = request.form.get("BMI")
-        bp = request.form.get("DP")
+        pedigree = request.form.get("DP")
         age = request.form.get("Age")
-        
+
+        # Storing variables in list.
+        variables = [times_pregnant, glucose_concentration, bp, skin_fold, serum_insulin, bmi, pedigree, age]
+        # Passing variables through model.
+        result = model.predict([variables])
+
+        if result[0] == 0:
+            diagnosis = "Negative"
+        else:
+            diagnosis = "Positive"
+
+        return(render_template("result.html", result_ = diagnosis))
 
 
 if __name__ == "__main__":
