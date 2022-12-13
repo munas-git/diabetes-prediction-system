0af412a41f991a373b28bf0399d4e0af0d88ac8d
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2022-12-13 22:07:18 +0100

    System build complete

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 # Deserialization of saved model.
 model = pickle.load(open("final_model.pkl", "rb"))
 
+session = {
+    "result": ""
+}
 
 
 @app.route("/home/", methods=["POST", "GET"])
@@ -30,13 +33,21 @@ def home():
         variables = [times_pregnant, glucose_concentration, bp, skin_fold, serum_insulin, bmi, pedigree, age]
         # Passing variables through model.
         result = model.predict([variables])
+        # Storing result in session.
+        session["result"] = result
+        return redirect(url_for("result"))
 
-        if result[0] == 0:
-            diagnosis = "Negative"
-        else:
-            diagnosis = "Positive"
 
-        return(render_template("result.html", result_ = diagnosis))
+@app.route("/result", methods= ["GET"])
+def result():
+    if session["result"] == 0:
+        diagnosis = "Negative"
+        message = "The doctor advices againsts heavy intake of heavily processed carbohydrates, sugar, red meat and processed meat. This would help reduce the risk of type 2 Diabetes"
+        return(render_template("result.html", result_ = diagnosis, message_ = message))
+    else:
+        diagnosis = "Positive"
+        message = "you are adised to schedule an appointment with a doctor close examination of your health situation. In order to avoid any further complications, you need to; Stop smoking if you currently smoke, keep your blood pressure and cholesterol under control, keep your vaccines up to date, pay attention to your feet, drink alcohol responsibly if you currently do so and take stress seriously."
+        return(render_template("result.html", result_ = diagnosis, message_ = message))
 
 
 if __name__ == "__main__":
